5a5d645074d204fd3e9089040e640aac476a54a6
mohammed <mohammdneccar1999@gmail.com>
2025-08-20 21:58:29 +0300

    first commit

---
+++ lib/homePage.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+
+class homPage extends StatefulWidget {
+  const homPage({super.key});
+
+  @override
+  State<homPage> createState() => _homPageState();
+}
+
+class _homPageState extends State<homPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+        padding: EdgeInsets.symmetric(horizontal: 20, vertical: 20),
+        child: ListView(children: [
+          
+        ])
+      ),
+    );
+  }
+}
---
+++ lib/main.dart
@@ -0,0 +1,20 @@
+import 'package:first_app/homePage.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatefulWidget {
+  const MyApp({super.key});
+
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(debugShowCheckedModeBanner: false, home: homPage());
+  }
+}
